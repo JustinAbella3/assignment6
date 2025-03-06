be90fbce74bb3f39dbd151812c509526990c22ba
Justin Abella <121201181+JustinAbella3@users.noreply.github.com>
2025-03-05 23:40:07 -0600

    Fixed File Structure.

--- src/test/java/BarnesAndNobleTest.java
+++ src/test/java/BarnesAndNobleTest.java
@@ -1,5 +1,4 @@
-package org.example.Barnes;
-
+import org.example.Barnes.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
